de70df49ffedd6806b09954d6eb4fdc98de517eb
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-24 15:58:30 +0200

    add initial commit snak 5 and create array 1-2

---
+++ Snak5/main.js
@@ -0,0 +1,35 @@
+/*Scrivi una funzione che accetti tre argomenti:
+un array e due numeri (“a” più piccolo di “b” e “b” grande al massimo
+quanto il numero di elementi dell’array).
+La funzione ritornerà un nuovo array con i valori che hanno la posizione
+compresa tra “a” e “b”*/
+
+//creo funzione con i 3 argomenti (arr e num , num)
+
+
+
+function myArr (element, firstNum, secondNum) {
+    
+    //creo primo array
+    const newArr = [];
+
+    for (let i = 0; i < 10; i++) {
+       
+        //chiedo se il numero a è più piccolo di b e inserisco l'eventuale numero dentro l'array
+
+        if (firstNum < element[i] && secondNum >= element[i]) {
+            newArr.push(elment[i])
+        }
+        
+    }
+    return newArr
+    
+} 
+
+//creo secondo array
+const firstArr =[];
+
+for (let i = 1; i <= 10; i++) {
+    firstArr.push(i);
+    
+}
