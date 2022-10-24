9e17e04309245d5eabf84adc43e040e1b6923693
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-24 16:19:44 +0200

    add initial commit bonus

---
+++ Bonus/main.js
@@ -0,0 +1,25 @@
+/*Bonus:
+Crea un array di oggetti che rappresentano delle persone.
+Ogni persona ha un nome, un cognome e un’età.
+Crea quindi un nuovo array inserendo, per ogni persona, 
+una frase con il nome e cognome e l’indicazione se può guidare, in base all’età.*/
+
+const users = [
+    {
+        nome: 'andrea',
+        cognome: 'russo',
+        età: 27,
+    },
+    {
+        nome: 'alessio',
+        cognome: 'russo',
+        età: 19,
+    },
+    {
+        nome: 'silvio',
+        cognome: 'russo',
+        età: 57,
+    },
+]
+
+const usersCar = [];
