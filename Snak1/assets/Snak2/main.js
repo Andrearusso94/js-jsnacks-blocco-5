39e1fd0ed0225e5388640dfdb16d5ecfe3f26f67
Andrearusso94 <andrea.r.4845@gmail.com>
2022-10-24 15:03:18 +0200

    initial commit snak2

---
+++ Snak1/assets/Snak2/main.js
@@ -0,0 +1,57 @@
+/*Crea un array di 10 oggetti che rappresentano una zucchina. 
+Dividi in due array separati le zucchine che misurano meno o più di 15cm. 
+Infine stampa separatamente quanto pesano i due gruppi di zucchine*/
+//creo array 10 zucchibe
+let zucchine = [
+    {
+                varieta: 'romanseche con fiore',
+                peso: 5,
+                lunghezza: 11
+             },
+             {
+                varieta: 'verdi',
+                peso: 25,
+                lunghezza: 13
+             },
+             {
+                varieta: 'verdi',
+                peso: 5,
+                lunghezza: 18
+             },
+             {
+                varieta: 'romanseche con fiore',
+                peso: 15,
+                lunghezza: 8
+             },
+             {
+                varieta: 'romanseche con fiore',
+                peso: 5,
+                lunghezza: 8
+             },
+             {
+                varieta: 'verdi',
+                peso: 25,
+                lunghezza: 12
+             },
+             {
+                varieta: 'verde con fiore',
+                peso: 5,
+                lunghezza: 14
+             },
+             {
+                varieta: 'spagnole',
+                peso: 15,
+                lunghezza: 16
+             },
+             {
+                varieta: 'romanesche',
+                peso: 5,
+                lunghezza: 10
+             },
+             {
+                varieta: 'romanesche',
+                peso: 10,
+                lunghezza: 18
+             },
+
+ ]
